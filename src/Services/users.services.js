beb6337cdf6e1f74bb85a2bfbc753a8a0850ec48
Tatiana_03 <89490161+miau12333@users.noreply.github.com>
2022-11-07 22:30:36 -0500

    Update users.services.js

--- src/Services/users.services.js
+++ src/Services/users.services.js
@@ -74,6 +74,14 @@ class UserServices {
       throw error;
     }
   }
+  static async add(newUser){
+    try {
+      const result = await Users.create(newUser);
+      return result;
+    } catch (error){
+      throw error;
+    }
+  }
 }
 
 module.exports = UserServices;
